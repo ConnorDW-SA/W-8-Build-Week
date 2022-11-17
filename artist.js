9703bd58d28df6ea8370adba401c9bac40730032
ConnorDW-SA <114667779+ConnorDW-SA@users.noreply.github.com>
2022-11-17 19:57:26 +0200

    .

--- artist.js
+++ artist.js
@@ -29,17 +29,45 @@ async function getAlbum() {
 
   const songList = album.tracks.data;
   console.log(songList);
-  // const indexPosition = $(".index-selector").index(this);
-  $(".song-list-container").each(function (i) {
-    this.innerHTML += `<div class="row justify-content-between my-4">
-                      
-                       <div class="row ml-5 text-left w-25 index-selector text-truncate"><img class="d-none" id="playing-gif" src="images/playing.gif" /><img
-                      class="mx-3 album-cover-small" src="${album.cover_small}" style="height: 60px" /><span class="mt-2">${songList[i].title}</span></div>
-                      <div class="rank mt-3 text-right w-25 text-truncate">Rank ${songList[i].rank}</div>
-                      <div class="mr-5 mt-3 text-left w-25 text-truncate">${songList[i].duration} seconds</div>
-                      </div>`;
-  });
 }
+let x = 0;
+let y = 3;
+loadSongs();
+
+async function loadSongs() {
+  const response = await fetch(
+    `https://striveschool-api.herokuapp.com/api/deezer/album/${album_id}`
+  );
+  const album = await response.json();
+
+  for (let i = x; i < y; i++) {
+    const container = document.querySelector(".song-list-container");
+    const row = document.createElement("div");
+    const songList = album.tracks.data;
+
+    row.innerHTML = `<div class="row justify-content-between my-4">
+                      
+                           <div class="row ml-5 text-left w-25 index-selector text-truncate d-block"><img class="d-none" id="playing-gif" src="images/playing.gif" /><img
+                          class="mx-3 album-cover-small" src="${album.cover_small}" style="height: 60px" /><span class="mt-2">${songList[i].title}</span></div>
+                          <div class="rank mt-3 text-right w-25 text-truncate ">Rank ${songList[i].rank}</div>
+                          <div class="mr-5 mt-3 text-left w-25 text-truncate ">${songList[i].duration} seconds</div>
+                         </div>`;
+    container.prepend(row);
+  }
+  x = y;
+  y = y + 20;
+}
+// const indexPosition = $(".index-selector").index(this);
+//   $(".song-list-container").each(function (i) {
+//     this.innerHTML += `<div class="row justify-content-between my-4">
+
+//                        <div class="row ml-5 text-left w-25 index-selector text-truncate d-block"><img class="d-none" id="playing-gif" src="images/playing.gif" /><img
+//                       class="mx-3 album-cover-small" src="${album.cover_small}" style="height: 60px" /><span class="mt-2">${songList[i].title}</span></div>
+//                       <div class="rank mt-3 text-right w-25 text-truncate ">Rank ${songList[i].rank}</div>
+//                       <div class="mr-5 mt-3 text-left w-25 text-truncate ">${songList[i].duration} seconds</div>
+//                       </div>`;
+//   });
+// }
 
 window.onload = () => {
   getArtists();
