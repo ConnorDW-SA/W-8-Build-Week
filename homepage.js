d5d73f627c48b28dc30c886d7a2489062b829b2c
Morgan <morgan@morganevans.dev>
2022-11-18 10:54:50 +0000

    Links to random album by artist

--- homepage.js
+++ homepage.js
@@ -6,6 +6,8 @@ let defaultSearch = "queen";
 
 const bigCardLayout = `col-md-2`;
 
+let artistsObj = {};
+
 const search = async (text) => {
     const { data } = await request.get(uri + `search?q=${text}`);
 
@@ -31,9 +33,21 @@ const search = async (text) => {
             (v, i, a) => a.findIndex((v2) => v2.id === v.id) === i
         );
 
-        randomAlbum = albums[Math.floor(Math.random() * albums.length)].id;
+        for (const song of songs) {
+            if (!artistsObj[song.artist.id]) {
+                artistsObj[song.artist.id] = [song.album.id];
+            } else {
+                artistsObj[song.artist.id].push(song.album.id);
+            }
+        }
+
+        // randomAlbum = albums[Math.floor(Math.random() * albums.length)].id;
 
         for (const artist of artists) {
+            let randomAlbum =
+                artistsObj[artist.id][
+                    Math.floor(Math.random() * artistsObj[artist.id].length)
+                ];
             let html = `<div class="${bigCardLayout}">
                 <a class="big-card artist" href="artistFinish.html?artist_id=${artist.id}&album_id=${randomAlbum}">
                     <img src="${artist.picture}" alt="${artist.name}">
@@ -59,6 +73,12 @@ const search = async (text) => {
 
         let fourSongs = songs.slice(1, 5);
         for (const song of fourSongs) {
+            let randomAlbum =
+                artistsObj[song.artist.id][
+                    Math.floor(
+                        Math.random() * artistsObj[song.artist.id].length
+                    )
+                ];
             let html = `<div class="song" onclick="playTrack('${song.id}');">
                 <img src="https://e-cdns-images.dzcdn.net/images/artist/${song.md5_image}/120x120-000000-80-0-0.jpg" alt="${song.title}" />
                 <div class="details">
@@ -77,6 +97,12 @@ const search = async (text) => {
             `https://e-cdns-images.dzcdn.net/images/artist/${featuredSong.md5_image}/120x120-000000-80-0-0.jpg`
         );
 
+        let randomAlbum =
+            artistsObj[featuredSong.artist.id][
+                Math.floor(
+                    Math.random() * artistsObj[featuredSong.artist.id].length
+                )
+            ];
         $(".showcase h3").text(featuredSong.title);
         $(".showcase .details a").text(featuredSong.artist.name);
         $(".showcase .details a").attr(
